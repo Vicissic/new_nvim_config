5da86fd4d1f9b18cfeefbf0a0ecf25c3c9f457c2
vicissic <victor.velesco@gmail.com>
2025-10-04 10:59:40 +0200

    fixing nvim config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,12 +1,20 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            }
-        }
-    }
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
+
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
+
+    local mason_tool_installer = require("mason-tool-installer")
+
+    -- enable mason and configure icons
+    mason.setup({
+    })
+  end,
 }
